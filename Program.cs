b630b49858f37a8bd8fa2582357fa30408b1f41a
alexeys6969 <alexshashin.2023@gmail.com>
2026-03-10 16:53:18 +0500

    Подключение стилей к страницам. Шашин

--- Program.cs
+++ Program.cs
@@ -2,14 +2,11 @@ using Shop.Data.Interfaces;
 using Shop.Data.Mocks;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 
 
 builder.Services.AddTransient<ICategorys, MockCaregories>();
 builder.Services.AddTransient<IItems, MockItems>();
-
-builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
-
-
 
 var app = builder.Build();
 
