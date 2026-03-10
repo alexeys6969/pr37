7bda78362c266e032f6ee99359a50bff960c93b3
alexeys6969 <alexshashin.2023@gmail.com>
2026-03-10 15:21:56 +0500

    Разработанные модели данных. Шашин

---
+++ Data/Models/Categories.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Data.Models
+{
+    public class Categories
+    {
+        /// <summary> ID товара
+        public int Id { get; set; }
+        /// <summary> Наименование товара
+        public string Name { get; set; }
+        /// <summary> Описание товара
+        public string Description { get; set; }
+        public  List<Items> Items { get; set; }
+    }
+}
