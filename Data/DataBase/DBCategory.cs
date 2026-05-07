1ec9bfe948811a2f4a4a92223f8286fed5ad3256
alexeys6969 <alexshashin.2023@gmail.com>
2026-05-07 09:01:09 +0500

    Получение данных о категориях. Шашин

---
+++ Data/DataBase/DBCategory.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+
+namespace Shop.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+        public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                List<Categories> categories = new List<Categories>();
+                MySqlConnection conn = Common.Connection.MySqlOpen();
+                MySqlDataReader CategoriesData = Common.Connection.MySqlQuery("SELECT * FROM Categorys ORDER BY `Name`;", conn);
+                while (CategoriesData.Read())
+                {
+                    categories.Add(new Categories()
+                    {
+                        Id = CategoriesData.IsDBNull(0) ? -1 : CategoriesData.GetInt32(0),
+                        Name = CategoriesData.IsDBNull(1) ? null : CategoriesData.GetString(1),
+                        Description = CategoriesData.IsDBNull(1) ? null : CategoriesData.GetString(2)
+                    });
+                }
+                return categories;
+            }
+        }
+    }
+}
