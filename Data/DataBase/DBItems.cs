9e90764fac98be266ac5f2baddafc212e5785e0c
alexeys6969 <alexshashin.2023@gmail.com>
2026-05-07 09:06:05 +0500

    Получение данных о предметах. Шашин

---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+
+namespace Shop.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categories> Categories = new DBCategory().AllCategories;
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection conn = Common.Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Common.Connection.MySqlQuery("SELECT * FROM Items ORDER BY `Name`;", conn);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categories.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                return items;
+            }
+        }
+    }
+}
